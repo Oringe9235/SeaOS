b474a042c81d4d14eefc5f092087b215e6f7cecf
Oringe9235 <97349567+Oringe9235@users.noreply.github.com>
2022-07-14 01:43:58 +0800

    Create Massages.asm
    SeaOS的诞生

---
+++ Massages.asm
@@ -0,0 +1,19 @@
+org 07c00h
+mov ax,cs
+mov ds,ax
+mov es,ax
+call start
+jmp $
+start:
+    mov ax, Massage
+    mov bp,ax
+    mov cx,16
+    mov dl,0
+    mov bx,000ch
+    mov ax,1301h
+    int 10h
+    ret
+Massage: db 'SeaOS, was born!'
+times 510 - ($ - $$) db 0
+dw 0xaa55
+
