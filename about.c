061d0d860e8b06806492d94c02942feb61723cbb
Oringe9235 <97349567+Oringe9235@users.noreply.github.com>
2022-07-15 02:06:26 +0800

    kernelFile

---
+++ about.c
@@ -0,0 +1,32 @@
+#include "stringio.h"
+
+void about()
+{
+    char* logo_1 = " ____                     _____   ____        ";
+    char* logo_2 = "/\  _`\                  /\  __`\/\  _`\      ";
+    char* logo_3 = "\ \,\L\_\     __     __  \ \ \/\ \ \,\L\_\    ";
+    char* logo_4 = " \/_\__ \   /'__`\ /'__`\ \ \ \ \ \/_\__ \    ";
+    char* logo_5 = "   /\ \L\ \/\  __//\ \L\.\_\ \ \_\ \/\ \L\ \  ";
+    char* logo_6 = "   \ `\____\ \____\ \__/.\_\\ \_____\ `\____\ ";
+    char* logo_7 = "    \/_____/\/____/\/__/\/_/ \/_____/\/_____/ ";
+    char* title =  "--SeaOS Version 1.0";
+    char* copyRight = "--copyright by GPLv2";
+    char* projectAddr = "https://github.com/Oringe9235/SeaOS";
+    
+    print(logo_1);
+    print(logo_2);
+    print(logo_3);
+    print(logo_4);
+    print(logo_5);
+    print(logo_6);
+    print(logo_7);
+    print(title);
+    print(copyRight);
+    print(projectAddr);
+}
+
+
+
+
+
+
