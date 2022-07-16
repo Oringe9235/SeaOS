05cafd8142a599ce5ebad97624a2f59a9d04ede4
Oringe9235 <97349567+Oringe9235@users.noreply.github.com>
2022-07-16 20:07:50 +0800

    Update com.sh

--- SeaOS v1.1/com.sh
+++ SeaOS v1.1/com.sh
@@ -5,12 +5,12 @@ chmod 777 ./bin;
 nasm -f elf32 ./kernel.asm -o ./bin/kernel.o;
 nasm -f elf32 ./liba.asm -o ./bin/liba.o;
 
-gcc -c -m -march=i386 -masm=intel -nostdlib -ffreestanding -mpreferred-stack-boundary=2 -lgcc -shared libc.c -o ./bin/libc.o;
+gcc -c -march=i386 -masm=intel -nostdlib -ffreestanding -mpreferred-stack-boundary=2 -lgcc -shared libc.c -o ./bin/libc.o;
 
-chmod 777 ./bin/OSkernel.o;
+chmod 777 ./bin/kernel.o;
 chmod 777 ./bin/liba.o;
 chmod 777 ./bin/libc.o;
 
-ld -m elf_i386 -T -N -Ttext  0x8000 --oformat binary  ./bin/kernel.o  ./bin/liba.o  ./bin/libc.o -o bin/osKernel.bin;
+ld -m elf_i386 -N -Ttext  0x8000 --oformat binary  ./bin/kernel.o  ./bin/liba.o  ./bin/libc.o -o bin/osKernel.bin;
 
 echo "Done!";
